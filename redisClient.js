811ff603060c5e53eaa0a65839f334a07b1eef8e
Anish Singh Walia <loveeanish@gmail.com>
2024-05-21 18:19:21 +0530

    Update redisClient.js

--- redisClient.js
+++ redisClient.js
@@ -8,14 +8,6 @@ function createRedisClient() {
       host: 'db-redis-blr1-55103-do-user-13729304-0.c.db.ondigitalocean.com',
       port: 25061,
       password: 'AVNS_3Sj9qjkIRWdTG_4UIph'
-      maxRetriesPerRequest: null, // Disable this limit
-      retryStrategy: (times) => {
-        if (times > 10) {
-          // Stop retrying after 10 attempts
-          return null;
-        }
-        return Math.min(times * 50, 2000);
-      },
     });
 
     redisInstance.on('connect', () => {
